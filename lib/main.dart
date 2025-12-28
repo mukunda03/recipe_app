eec4235c832644b5289a3238fd9adb9ba0bca0c2
mukunda03 <mukundasv03@gmail.com>
2025-12-28 16:25:10 +0530

    Add app icon configuration for Android

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'demo',
+      title: 'Gourmet Guide',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const SplashScreen(),
     );
